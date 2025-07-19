7ac945e6a0b04c3d247b65563f68d3d6eeefc6d5
Tony Howell <anhwell@amazon.com>
2025-07-19 15:41:21 -0500

    cleanup

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -5,9 +5,6 @@ import { CdkStack } from '../lib/cdk-stack';
 
 const app = new cdk.App();
 
-// Get the deployment mode from context or environment variable
-const deploymentMode = app.node.tryGetContext('deploymentMode') || process.env.DEPLOYMENT_MODE || 'ecs';
-
 new CdkStack(app, 'AWS-GENAI-UW-DEMO', {
   description: 'AWS Underwriting Assistant'
 });
